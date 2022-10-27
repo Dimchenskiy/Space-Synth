29de4f62cb305259baa339743b7f3475b9a34263
Dimchenskiy <47354895+Dimchenskiy@users.noreply.github.com>
2022-10-27 10:06:11 +0300

    Add files via upload

---
+++ db/seeds.rb
@@ -0,0 +1,61 @@
+@prototypes_data = [
+  {
+    name: 'First module / First prototype'
+  },
+  {
+    name: 'First oscillator'
+  },
+  {
+    name: 'Oscillator with React components'
+  },
+  {
+    name: 'Tone.js basics'
+  },
+  {
+    name: 'My First Melody'
+  },
+  {
+    name: 'My Second Melody'
+  },
+  {
+    name: 'My Third Melody'
+  },
+  {
+    name: 'Tone.js with UI'
+  },
+  {
+    name: 'Tone.js with UI, better change function'
+  },
+  {
+    name: 'Tone.js with UI, composition'
+  },
+  {
+    name: 'Tone.js with UI, sampler'
+  },
+  {
+    name: 'Knob in UI, Show/Hide Instrument, Show/Hide UI Parts'
+  },
+  {
+    name: 'Project 1st module'
+  }
+]
+
+def seed
+  reset_db
+  create_prototypes
+end
+
+def reset_db
+  Rake::Task['db:drop'].invoke
+  Rake::Task['db:create'].invoke
+  Rake::Task['db:migrate'].invoke
+end
+
+def create_prototypes
+  @prototypes_data.each do |prototype_data|
+    prototype = Prototype.create!(prototype_data)
+    puts "#{prototype.name} prototype just created"
+  end
+end
+
+seed
